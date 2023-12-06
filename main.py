01883e5b616049f16e5f4e7c300eddd269d5b72a
wjcwqc <wjcwqc@gmail.com>
2023-12-06 15:45:30 +0800

    first commit

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from random import *
+import sys
 
 import ddddocr
 import requests
 
 from staticres import *
 
+# 验证码绕过
 def verify_ocr(imgstr):
     ocr = ddddocr.DdddOcr(beta=True, show_ad=False)
     with open("imgfile.gif", 'wb') as file:
         file.write(imgstr)
         file.close()
-    # print(imgstr)
     res = ocr.classification(imgstr)
     return res
 
-
-def send_sms(region, phone):
+# 发送短信
+def send_sms(region, phone,sms="update+validation"):
     # cookie = {"ASP.NET_SessionId": ''.join([choice(comstr) for i in range(24)])}
-    # print(cookie)
-    sms = "update+validation"
     # print(cookie)
     verify=requests.get(imgurl)
     # code = verify_ocr(verify.content)
@@ -28,19 +27,24 @@ def send_sms(region, phone):
     rst = requests.post(shiyongurl, headers=header, cookies=cookie, data="id={}&phone={}&content={}&vcode={}".format(
         next((element for element in regionlist if element["CountryCode"] == region), None)['Id'], region + phone, sms,
         code)).text
-    print(rst)
+    return rst
+
+# 检擦发送状态
+def check(rst):
     if rst.find("status=") == 0:
         rst = int(rst.split("=")[1])
-    if rst > 80000:
-        return True
-    elif rst < 0:
-        return False
+        if rst > 80000:
+            return True
+        else:
+            return rst
     else:
         return rst
 
 
+
 def main():
-    rst = send_sms("48", "699527124")
+    region,phone=sys.argv[1:]
+    rst = check(send_sms(region, phone))
     if rst == True:
         print("True")
     elif rst == False:
@@ -49,10 +53,5 @@ def main():
         print("rst: ", rst)
 
 
-def test(region):
-    return next((element for element in regionlist if element["CountryCode"] == region), None)
-
-
 if __name__ == '__main__':
     main()
-    # print(test("48"))
